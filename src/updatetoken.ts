279855dd0edb5717643ce30036655a4ff1bf64a9
Савелій Савёнак <1986developer@gmail.com>
2023-05-05 16:58:59 +0300

    Fix save config token

--- src/updatetoken.ts
+++ src/updatetoken.ts
@@ -2,10 +2,10 @@ import * as vscode from 'vscode';
 
 export default function(){
     let options: vscode.InputBoxOptions = {
-        prompt: "Bearer token from https://huggingface.co"
+        prompt: "Insert bearer token from https://huggingface.co"
     };
     vscode.window.showInputBox(options).then(value => {
-        vscode.workspace.getConfiguration("starcoderex").update("bearertoken", value);
+        vscode.workspace.getConfiguration("starcoderex").update("bearertoken", value, vscode.ConfigurationTarget.Global);
     }); 
     return;
 }
