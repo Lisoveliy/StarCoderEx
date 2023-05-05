f4602dc99375e580199688b198f6d2362a5e3371
Gleb <M20XV@yandex.ru>
2023-05-05 21:46:58 +0300

    added browser open on update token

--- src/updatetoken.ts
+++ src/updatetoken.ts
@@ -1,11 +1,15 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-export default function(){
-    let options: vscode.InputBoxOptions = {
-        prompt: "Insert bearer token from https://huggingface.co"
-    };
-    vscode.window.showInputBox(options).then(value => {
-        vscode.workspace.getConfiguration("starcoderex").update("bearertoken", value, vscode.ConfigurationTarget.Global);
-    }); 
-    return;
+export default function () {
+  let options: vscode.InputBoxOptions = {
+    prompt: "Insert bearer token from https://huggingface.co",
+  };
+  vscode.commands
+    .executeCommand("vscode.open", vscode.Uri.parse("https://huggingface.co"))
+    .then(() => vscode.window.showInputBox(options))
+    .then((value) => {
+      vscode.workspace
+        .getConfiguration("starcoderex")
+        .update("bearertoken", value, vscode.ConfigurationTarget.Global);
+    });
 }
